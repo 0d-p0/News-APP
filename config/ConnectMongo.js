2d508d0bc4c07eb4beda9e7202303907356b28a9
0d-p0 <maiti7211442001@gmail.com>
2023-03-28 23:26:15 +0530

    update test 0.3 vercel

--- config/ConnectMongo.js
+++ config/ConnectMongo.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.connectDatabase = () => {
   mongoose
-    .connect(`mongodb+srv://pritam:${process.env.password}@news1.hujrnsz.mongodb.net/shortNews`)
+    .connect(`mongodb+srv://vercel-admin-user:${process.env.password}@news1.hujrnsz.mongodb.net/shortNews`)
     .then((con) => console.log("database connected ", con.connection.host))
     .catch((err) => console.error(err));
 };
